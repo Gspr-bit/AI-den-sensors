ff54ce3dfa66437126a44656fa0319a2aecbb827
Huerta Saavedra <gasparhs400@gmail.com>
2024-04-01 16:09:42 -0600

    Sensor de temperatura y humedad del aire

--- Sensors/Sensors.ino
+++ Sensors/Sensors.ino
@@ -1,18 +1,23 @@
+#include "DHT.h"
+
 const int AirQualitySensorPin = A0;
 const int GasPresencePin = 2;
+const int DhtPin = 3;
 
 int airQualityData;
 int particlesFound;
 
+DHT dht(DhtPin, DHT11);
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  dht.begin();
   pinMode(AirQualitySensorPin, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void getPPM() {
   airQualityData = analogRead(AirQualitySensorPin);
   particlesFound = digitalRead(GasPresencePin);
 
@@ -22,6 +27,23 @@ void loop() {
   } else {
     Serial.println("No particles found");
   }
+}
+
+void getAirInfo() {
+  float airHumidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  Serial.print("Humidity:");
+  Serial.println(airHumidity);
+
+  Serial.print("Temperature:");
+  Serial.println(temperature);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  getPPM();
+  getAirInfo();
   
   digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
